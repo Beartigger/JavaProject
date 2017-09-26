794fe79a43c0a650a73f2505f279c0292bea5522
Beartigger <user@beartigger6.mylabserver.com>
2017-09-26 18:55:07 +0000

    first Push

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+   public int length;
+   public int width;
+   
+   public Rectangle (int length, int wideth) {
+      this.length = length;
+	  this.width = width;
+	}
+
+   public int getArea () {
+      return length * width;
+	}
+   
+   public int getParimeter () {
+      return 2 * (length + width);
+	}
+}
+
