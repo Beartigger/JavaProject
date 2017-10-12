e220b47b32622c0cbfe01073ed710b09cb079347
beartigger <dominic_king@fastmail.net>
2017-10-12 14:46:54 -0400

    fix test.java

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RectangleTest {
-	Rectangle MyRectangle = new Rectangle(5,6);
+	Rectangle myRectangle = new Rectangle(5,6);
 	@Test
 	public void testGetArea() {
 		assertEquals(myRectangle.getArea(), 30);
